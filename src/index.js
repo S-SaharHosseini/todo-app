1278f6fe6b5c7f8f705aa00f83a7e1268001bbbb
S-SaharHosseini <s.sahar.hosseini98@gmail.com>
2024-08-26 21:41:15 +0330

    todo app template

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./assets/css/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Todo app</h1>);
+root.render(
+  <div className="main">
+    <div className="h-4/6 w-full todo-main flex justify-center">
+      <h1 className="absolute left-10 top-16 text-9xl">Todo <br></br> App</h1>
+      <div className="h-screen sm:w-10/12 lg:w-5/12 mt-10 rounded-tl-full rounded-tr-full todo-wrapper"></div>
+    </div>
+    <div className="h-2/6 w-full todo-footer"></div>
+  </div>
+);
