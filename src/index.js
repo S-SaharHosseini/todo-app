e601e8280f5a9a7700895fa512250a31ce956823
S-SaharHosseini <s.sahar.hosseini98@gmail.com>
2024-08-27 11:19:02 +0330

    todo template & responsive

--- src/index.js
+++ src/index.js
@@ -9,7 +9,28 @@ root.render(
       <h1>
         Todo <br></br> App
       </h1>
-      <div className="todo-wrapper"></div>
+      <div className="todo-wrapper">
+        <form>
+          <input type="text" placeholder="add new todo..." />
+          <button>
+            <i className="fi fi-rs-plus"></i>
+          </button>
+        </form>
+        <div className="todo-list-wrapper">
+          <div className="todo-list">
+            <div className="todo-list-item group">
+              <div className="todo-list-item-text">
+                <p>first todo for test</p>
+              </div>
+              <div className="todo-action ">
+                <i className="fi fi-rs-trash"></i>
+                <i className="fi fi-rs-cross-small"></i>
+                <i className="fi fi-rs-check"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div className="todo-footer"></div>
   </div>
